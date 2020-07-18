a861772bf7e89dc25190931e031b0fed88116a8f
daveshuford <68476522+daveshuford@users.noreply.github.com>
2020-07-18 13:04:51 -0500

    Update db.py

--- feed_parser/db.py
+++ feed_parser/db.py
@@ -3,7 +3,7 @@ CONNECT TO SQL_DB AND WRITE TO TABLE
 '''
 from sqlalchemy import create_engine
 
-engine = create_engine('mysql+pymysql://root:Shuford@00536898@localhost/rss_feed')
+engine = create_engine('mysql+pymysql://root:***@localhost/rss_feed')
 
 def write2sql(df):
     df.to_sql('article_feed', con = engine, if_exists = 'append', chunksize = 1000, index=False)
